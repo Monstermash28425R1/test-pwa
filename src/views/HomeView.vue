0e148af7fb3b024b1e4c336d57d91e6b49e585a2
Miguel <theregressor28@gmail.com>
2022-07-30 14:54:22 -0400

    project configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+    import { useMeta } from "vue-meta";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        setup() {
+            useMeta({
+                title: "Home",
+            });
+        },
+        components: {
+            HelloWorld,
+        },
+    };
 </script>
